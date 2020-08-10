d7b88d254073336cf1c68ba75b6b404ad5a8356b
Ze <josericardodainese@gmail.com>
2020-08-10 02:38:37 -0300

    NLW Finalizado Front Pega Total Conexões

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg'
@@ -9,8 +9,19 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
 import './style.css'
+import api from '../../services/api';
 
   function Landing() {
+
+    const [totalConnectionns, setTotalConnectionns] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data;
+            setTotalConnectionns(total);
+        });
+    }, []);
+
       return (
         <div id="page-landing">
             <div id="page-landing-content">
@@ -36,7 +47,7 @@ import './style.css'
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas 
+                    Total de {totalConnectionns} conexões já realizadas 
                     <img src={purpleHeartIcon} 
                     alt="Coração Roxo"/>
                 </span>
